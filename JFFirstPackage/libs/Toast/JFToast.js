fa68b401c2aa24a2a50a48f3b8ad02e3377f87e1
jinfeng.du <jinfeng.du@qunar.com>
2016-09-13 17:45:54 +0800

    init rn package

---
+++ JFFirstPackage/libs/Toast/JFToast.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @providesModule JFToast
+ */
+
+'use strict';
+
+class JFToast extends Component {
+    render() {
+        return (
+            <Text style={{color: 'black',backgroundColor: 'red', width: 100, height: 30}}>
+                "JFToast 需要实现，用于测试"
+            </Text>
+        );
+    }
+};
+
+module.exports = JFToast;
